b116b06561c699d2d7cde117a67475743554c4c8
Anurag Shukla <160250034@ggi.ac.in>
2026-06-02 18:26:35 +0530

    Add initial HTML template for frontend application

--- frontend/src/services/employeeService.js
+++ frontend/src/services/employeeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || "https://emplyee-jbxe.onrender.com";
 
 export const getAllEmployees = async () => {
   return axios.get(`${API_URL}/employees`);
